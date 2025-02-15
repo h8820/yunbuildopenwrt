5fd1ffd0f62d1e8d2a274955d103b38821e2ced5
h8820 <101133120+h8820@users.noreply.github.com>
2025-02-15 11:38:52 +0800

    Update customfeeds.sh

--- customfeeds.sh
+++ customfeeds.sh
@@ -6,3 +6,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/h8820/package-1.git package/1806
